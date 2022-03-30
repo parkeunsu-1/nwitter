5e4336a8b8a2babcedd4d80a6710375dac64e611
parkeunsu-1 <tkfkdgo77855@naver.com>
2022-03-30 21:48:14 +0900

    new

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-
+import App from "./conponents/App";
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+console.log(firebase);
 
 ReactDOM.render(
   <React.StrictMode>
