0fe3b7d845894afa8ad7954773fd9a6116f23cb3
parkeunsu-1 <tkfkdgo77855@naver.com>
2022-04-06 20:13:16 +0900

    new

--- src/conponents/App.js
+++ src/conponents/App.js
@@ -1,8 +1,15 @@
+import {useState} from "react";
 import AppRouter from "./Router";
 
 function App() {
+   const [isLoggedIn, setIsLoggedIn]=useState(false);
 
-   return <AppRouter />;
+   return (
+   <>
+      <AppRouter isLoggedIn={isLoggedIn} />
+      <footer>&copy; {new Date().getFullYear()} Nwitter</footer>
+   </>
+   );
 
 }
 
