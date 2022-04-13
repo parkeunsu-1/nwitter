e4b3ebb48f022bb2731d30f37a4212ac8a0fc5c2
parkeunsu-1 <tkfkdgo77855@naver.com>
2022-04-13 21:10:24 +0900

    new

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,2 +1,16 @@
-const Auth = () => <span>Auth</span>;
+const Auth = () => {
+    return (
+        <div>
+            <form>
+                <input type="email" placeholder="Email" required />
+                <input type="password" placeholder="Password" required />
+                <input type="submit" value="Log In" />
+            </form>
+            <div>
+                <button>Continue with Google</button>
+                <button>Continue with Github</button>
+            </div>
+        </div>
+    );
+};
 export default Auth;
