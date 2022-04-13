52277e840a8d076a92798c8a5e6470b874ab3e93
parkeunsu-1 <tkfkdgo77855@naver.com>
2022-04-14 01:48:37 +0900

    new_4/14

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,10 +1,50 @@
+import { useState } from "react"
+
 const Auth = () => {
+    const [email, setEmail ] = useState("");
+    const [password, setPassword] = useState("");
+    const [newAccount, setNewAccount] = useState(true);
+
+
+    const onChange = (event) => {
+        const {
+            target : { name, value },
+        } = event;
+        if(name==="email"){
+            setEmail(value);
+        } else if (name==="password"){
+            setPassword(value);
+        }
+    };
+    const onSubmit = (event) => {
+        event.preventDefault();
+        if(newAccount){
+
+        }else {
+
+        }
+    };
+
     return (
         <div>
-            <form>
-                <input type="email" placeholder="Email" required />
-                <input type="password" placeholder="Password" required />
-                <input type="submit" value="Log In" />
+            <form onSubmit={onSubmit}>
+                <input 
+                    name="email"
+                    type="text"
+                    placeholder="Email"
+                    required
+                    value={email}
+                    onChange={onChange}
+             />
+                <input 
+                    name="password"
+                    type="password"
+                    placeholder="Password"
+                    required
+                    value={password}
+                    onChange={onChange}
+                 />
+                <input type="submit" value={newAccount ? "Create Account" : "Log In" }/>
             </form>
             <div>
                 <button>Continue with Google</button>
