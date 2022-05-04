c3e1814fd4526dda2ee219e93e264425881873bc
parkeunsu-1 <tkfkdgo77855@naver.com>
2022-05-04 21:35:35 +0900

    5/4 new

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,4 +1,4 @@
-import { authService } from "fbase";
+import { authService, firebaseInstance } from "fbase";
 import { useState } from "react"
 
 const Auth = () => {
@@ -37,8 +37,18 @@ const Auth = () => {
         }
     };
     const toggleAccount = () => setNewAccount((prev) => !prev);
-    const onSocialClick = (event) => {
-        console.log(event.target.name);
+    const onSocialClick = async (event) => {
+        const {
+            target : {name},
+        } = event;
+        let provider;
+        if(name === "google") {
+            provider = new firebaseInstance.auth.GoogleAuthProvider();
+        } else if(name === "github"){
+            provider = new firebaseInstance.auth.GithubAuthProvider();
+        }
+        const data = await authService.signInWithPopup(provider);
+        console.log(data);
     };
     return (
         <div>
