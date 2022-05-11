9f5cecadacaf526f42585fc1e5b2d4612b1a3999
parkeunsu-1 <tkfkdgo77855@naver.com>
2022-05-11 21:35:34 +0900

    5/11

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -3,14 +3,19 @@ import { useEffect, useState } from "react";
 
 const Home = () => {
     const [nweet, setNweet] = useState("");
+    const [nweets, setNweets ] =useState([]);
 
     const getNweets = async () => {
         const dbNweets = await dbService.collection("nweets").get();
-        dbNweets.forEach((document)=> console.log(document.data()));
+        dbNweets.forEach((document) => {
+            const nweetObject = { ...document.data(), id : document.id };
+            setNweets((prev)=> [nweetObject, ...prev])
+        });
     };
     useEffect(() => {
         getNweets();
     }, []);
+    console.log(nweets);
 
     const onSubmit =async (event) => {
         event.preventDefault();
