6b76c3df9cd3654d777cbd11a354b2dc67ce0e3b
parkeunsu-1 <tkfkdgo77855@naver.com>
2022-05-25 21:26:21 +0900

    5/25

--- src/components/Nweet.js
+++ src/components/Nweet.js
@@ -1,6 +1,9 @@
 import {dbService} from "fbase";
+import {useState} from "react";
 
 const Nweet =({nweetObj, isOwner})=>{
+    const [editing, setEditing] = useState(false);
+    const [newNweet, setNewNweet] = useState(nweetObj.text);
     const onDeleteClick =async () => {
         const ok = window.confirm("삭제하시겠습니까?");
         console.log(ok);
@@ -10,14 +13,41 @@ const Nweet =({nweetObj, isOwner})=>{
             console.log(data);
         }
     };
+    const toggleEditing = () => setEditing((prev)=> !prev);
+
+    const onChange = (event) => {
+        const {
+            target : {value},
+        } = event;
+        setNewNweet(value);
+        };
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        //console.log(nweetObj.id, newNweet);
+        await dbService.doc(`nweets/${nweetObj.id}`).update({text : newNweet});
+        setEditing(false);
+    };
     return (
         <div>
+            {editing ? (
+                <>
+                    <form onSubmit={onSubmit}>
+                        <input onChange={onChange} value = {newNweet} required />
+                        <input type ="submit" value="Update Nweet" />
+                    </form>
+                    <button onClick = {toggleEditing}>Cancel</button>
+                </>
+            ) : (
+        <>
+            
             <h4>{nweetObj.text}</h4>
             {isOwner && (
                 <>
                     <button onClick ={onDeleteClick}>Delete Nweet</button>
-                    <button>Edit Nweet</button>
+                    <button onClick={toggleEditing}>Edit Nweet</button>
                 </>
+            )}
+            </>
             )}
         </div>
     );
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -6,6 +6,7 @@ const Home = ({userObj}) => {
 
     const [nweet, setNweet] = useState("");
     const [nweets, setNweets ] =useState([]);
+    const [attachment, setAttachment]= useState("");
 
 //    const getNweets = async () => {
 //        const dbNweets = await dbService.collection("nweets").get();
@@ -42,6 +43,22 @@ const Home = ({userObj}) => {
         } = event;
         setNweet(value);
     };
+    const onFileChange = (event) => {
+
+        const {
+            target : {files},
+        } = event;
+        const theFile = files[0];
+        const reader = new FileReader();
+        reader.onloadend= (finishedEvent) => {
+            const {
+                currentTarget : {result},
+            } = finishedEvent;
+            setAttachment(result);
+        };
+        reader.readAsDataURL(theFile);
+    };
+    const onClearAttachment = () => setAttachment("");
     return (
         <>
         <form onSubmit = {onSubmit}>
@@ -52,7 +69,14 @@ const Home = ({userObj}) => {
             placeholder="What's on your mind?"
             maxLength={120}
             />
+            <input type="file" accept="image/*" onChange={onFileChange}/>
             <input type="submit" value="Nweet" />
+            {attachment && (
+            <div>
+                <img src ={attachment} width="50px" height="50px" />
+                <button onClick={onClearAttachment}>Clear</button>
+                </div>
+            )}
         </form>
         <div>
             {nweets.map((nweet)=>(
