a800240021dd42b8dfbed40621bf4d9e62467271
parkeunsu-1 <tkfkdgo77855@naver.com>
2022-06-14 21:22:55 +0900

    6/14

--- src/components/Router.js
+++ src/components/Router.js
@@ -5,11 +5,11 @@ import Home from "routes/Home";
 import Profile from "routes/Profile";
 import Navigation from "./Navigation";
 
-const AppRouter = ({ isLoggedIn, userObj }) => {
+const AppRouter = ({ isLoggedIn, userObj, refreshUser }) => {
 
     return (
         <Router>
-            {isLoggedIn && <Navigation />}
+            {isLoggedIn && <Navigation userObj={userObj} />}
             <Switch>
                 { isLoggedIn ? (
                     <>
@@ -17,7 +17,7 @@ const AppRouter = ({ isLoggedIn, userObj }) => {
                            <Home userObj={userObj}/>
                      </Route>
                      <Route exact path ="/profile">
-                         <Profile userObj={userObj}/>
+                         <Profile refreshUser={refreshUser} userObj={userObj}/>
                       </Route>
                     </>
                 ) : (
